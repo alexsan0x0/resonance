5bb92b53588d0ee0be492529d371c2b766dabcc5
AlexSan <ymoha4852@gmail.com>
2026-04-19 23:18:44 +0300

    feat: added clerk to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
   return (
-    <Button>
+    <Button onClick={() => toast.success("Hello, world!")}>
       <a href="https://www.google.com" target="_blank">
         Click me
       </a>
